a23797dc16e212734fe9bc611677660dc5a90f99
wonphe <wonphe@gmail.com>
2018-06-28 09:19:09 +0800

    添加菜单

--- app/src/main/java/top/a0x16/firstandroiddemo/MainActivity.java
+++ app/src/main/java/top/a0x16/firstandroiddemo/MainActivity.java
@@ -2,6 +2,9 @@ package top.a0x16.firstandroiddemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove", Toast.LENGTH_LONG).show();
+                break;
+            default:
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
